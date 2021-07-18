de8b50296f3b6f0dea0322ed57b711ef246d78a3
Anwesh43 <anweshthecool0@gmail.com>
2021-07-18 18:14:12 +0530

    adding DrawingUtil class

--- index.ts
+++ index.ts
@@ -25,3 +25,47 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawSideSquareRot(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        context.save()
+        context.translate(w / 2 + (w / 2 + size / 2) * sc4, h / 2 + (h / 2 - size)  * (1 - sc2))
+        context.rotate(Math.PI * sc2)
+        for (var j = 0; j < 2; j++) {
+            context.save()
+            context.scale(1 - 2 * j, 1)
+            if (sc1 > 0) {
+                DrawingUtil.drawLine(context, size / 2, size, size / 2, size - size * sc1)
+                context.restore()
+            }
+            if (sc3 > 0) {
+                context.save()
+                context.scale(1, 1 - 2 * j)
+                DrawingUtil.drawLine(context, size / 2, size / 2, size / 2 - size * sc3, size / 2)
+                context.restore()
+            }
+            context.restore()
+        }
+        context.restore()
+    }
+
+    static drawLSSRNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawSideSquareRot(context, scale)
+    }
+}
