18fea9936df37c350cae19ccc57c28c326bc3b7d
Anwesh43 <anweshthecool0@gmail.com>
2021-07-18 18:36:18 +0530

    adding LSSRNode class

--- index.ts
+++ index.ts
@@ -144,3 +144,47 @@ class Animator {
         }
     }
 }
+
+class LSSRNode {
+
+    prev : LSSRNode 
+    next : LSSRNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LSSRNode(this.i + 1)
+            this.next.prev = this  
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawLSSRNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : LSSRNode {
+        var curr : LSSRNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+
+
+}
